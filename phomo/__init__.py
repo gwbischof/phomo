351b88d31aba666bb31ec6bf1360e18c92c03a98
gbischof <bisc8233@gmail.com>
2020-10-18 15:28:33 -0400

    generate_random_corpus test

--- phomo/__init__.py
+++ phomo/__init__.py
@@ -9,7 +9,8 @@ __version__ = get_versions()['version']
 del get_versions
 
 
-def generate_random_corpus(k=2, num_letters=10, num_restrictions=5, max_word_length=10, corpus_length=1000):
+def generate_random_corpus(k=2, num_symbols=10, num_restrictions=5,
+                           max_word_length=10, corpus_length=1000):
 
     def make_word(max_length, symbols, allowed_dictionary):
         # How long is the word going to be?
@@ -17,29 +18,27 @@ def generate_random_corpus(k=2, num_letters=10, num_restrictions=5, max_word_len
         word = [random.choice(list(symbols))]
         for i in range(length-1):
             word.append(random.choice(list(allowed_dictionary[word[-1]])))
-            
-        # Converts the list to a string (word)
-        word = ''.join([item for t in word for item in t])
-        
-        return word
-    
-    def make_corpus(max_word_length, corpus_length, symbols, allowed_dictionary):
-        return " ".join([make_word(max_word_length, symbols, allowed_dictionary) for i in range(corpus_length)])
 
-    # TODO: Update letters with IPA symbols. 
+        # Converts the list to a string.
+        word = ''.join([item for t in word for item in t])
+
+        return word
+
+
+    # TODO: Update letters with IPA symbols.
     letters = list(string.ascii_lowercase)
-    symbols = set([random.choice(letters) for i in range(num_letters)])
+    symbols = set([random.choice(letters) for i in range(num_symbols)])
 
     symbol_permutations = list(itertools.permutations(symbols, 2))
     for symbol in symbols:
         symbol_permutations.append((symbol, symbol))
-        
+
     restrictions = set([random.choice(symbol_permutations) for i in range(num_restrictions)])
     allowed = set(symbol_permutations) - set(restrictions)
 
     allowed_dictionary = defaultdict(set)   #each key's value is a default set
     for first, second in allowed:
-        allowed_dictionary[first].add(second)  
+        allowed_dictionary[first].add(second)
     allowed_dictionary = dict(allowed_dictionary)
 
     grammar = {"symbols":  (len(symbols), symbols),
@@ -47,15 +46,11 @@ def generate_random_corpus(k=2, num_letters=10, num_restrictions=5, max_word_len
                "allowed": (len(allowed), allowed),
                "allowed_dictionary": allowed_dictionary}
 
-    corpus = make_corpus(max_word_length, corpus_length, symbols, allowed_dictionary)
+    corpus = " ".join([make_word(max_word_length, symbols, allowed_dictionary)
+                      for i in range(corpus_length)])
 
-    return grammar, corpus  
+    return grammar, corpus
 
-def a():
-    return 5
-
-def b():
-    return 5
 
 def classify_languages (sample_text, classifier):
     return predicted_language
--- phomo/tests/test_generate_corpus.py
+++ phomo/tests/test_generate_corpus.py
@@ -1,7 +1,8 @@
-from phomo import a, b, generate_random_corpus
+from phomo import generate_random_corpus
 
-def test_ab():
-    assert a() == b() 
+
+def test_generate_random_corpus():
+    grammar, corpus = generate_random_corpus()
 
 """
 def test_classifier():
@@ -12,7 +13,7 @@ def test_classifier():
     test_data = corpus1_slice2 + corpus2_slice2
 
     classifier = train_classifier(training_data)
-    
+
     accuracy = classifier.classify(test_data)
 
     print(accuracy)
